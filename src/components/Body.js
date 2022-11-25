f9b954dbdea8737d9e02490dc23e748a29343fc4
saurabhppawar04 <78255828+saurabhppawar04@users.noreply.github.com>
2022-11-25 19:14:14 +0530

    5th

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ import RightBody from "./Rightbody"
 
 function Body() {
     return (
-        <div className='d-flex my-0' style={{height: "90vh", width: '100vw'}}>
+        <div className='d-flex my-0' style={{height: "90vh", width: '99vw'}}>
             <LeftBody />
             <RightBody />
         </div>
